b82741c9298c8f92cf83bb922583ca80cc5ae477
Ilia <istudyatuni@gmail.com>
2021-03-15 21:08:47 +0300

    [server] Add router for adding task
    Add (not full) api in openapi.yaml

--- server/lib/tasker/router.ex
+++ server/lib/tasker/router.ex
@@ -3,14 +3,27 @@ defmodule Tasker.Router do
   use Plug.Router
   use Plug.Debugger
 
+  alias Tasker.Task
+
   require Logger
   plug(Plug.Logger, log: :debug)
 
   plug(:match)
   plug(:dispatch)
 
-  get "/api/:name" do
-    send_resp(conn, 200, name)
+  post "/api/task" do
+    {:ok, body, conn} = read_body(conn)
+    body = Poison.decode!(body)
+    {result, _} = Task.insert_changeset(body)
+    send_resp(conn, 200, Poison.encode!(result))
+  end
+
+  get "/api/task" do
+    # {:ok, body, conn} = read_body(conn)
+    # body = Poison.decode!(body)
+    # {result, _} = Task.insert_changeset(body)
+    result = "Not implemented"
+    send_resp(conn, 200, Poison.encode!(result))
   end
 
   match _ do
