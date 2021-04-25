885ecc3121942e85cbfeb21008f05734b5c2a07b
Ilia <istudyatuni@gmail.com>
2021-04-25 13:28:24 +0300

    [server] Remade serving static files (web dist), now with Plug.Static

--- server/lib/tasker.ex
+++ server/lib/tasker.ex
@@ -1,18 +1,3 @@
 defmodule Tasker do
-  @moduledoc """
-  Documentation for `Tasker`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tasker.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
--- server/lib/tasker/routers/web.ex
+++ server/lib/tasker/routers/web.ex
@@ -1,70 +1,26 @@
 defmodule Tasker.Routers.Web do
-  import Plug.Conn
-  use Plug.Router
-  use Plug.Debugger
+  use Plug.Builder
 
-  plug(:match)
-  plug(:dispatch)
+  @static_folder Path.expand(Application.get_env(:tasker, :web_app_folder))
 
-  def send_static_file(conn, folder, filename, mime_type) do
-    web_app_folder = Application.get_env(:tasker, :web_app_folder)
-    folder = "#{web_app_folder}#{folder}"
+  plug(Plug.Static,
+    at: "/",
+    from: @static_folder
+  )
 
-    if File.exists?("#{folder}/#{filename}") do
-      conn
-      |> put_resp_header("content-type", "#{mime_type}; charset=utf-8")
-      |> send_file(200, "#{folder}/#{filename}")
-    else
-      send_resp(conn, 404, "Not found")
-    end
-  end
+  plug(:not_found)
 
-  get "/" do
-    send_static_file(conn, "", "index.html", "text/html")
-  end
+  def not_found(conn, _) do
+    if conn.request_path == "/" do
+      file = "#{@static_folder}/index.html"
 
-  get "static/js/:name" do
-    send_static_file(conn, "/static/js", name, "application/javascript")
-  end
-
-  get "static/css/:name" do
-    send_static_file(conn, "/static/css", name, "text/css")
-  end
-
-  get "static/media/:name" do
-    mime_types = %{
-      "eot" => "application/octet-stream",
-      "png" => "image/png",
-      "svg" => "image/svg+xml",
-      "ttf" => "application/x-font-ttf",
-      "woff" => "application/font-woff",
-      "woff2" => "application/font-woff"
-    }
-
-    # extract from smth like [["name.png", "png"]]
-    ext = Regex.scan(~r/[\da-zA-Z]+\.([a-z]+)/, name) |> hd |> tl |> hd
-    send_static_file(conn, "/static/media", name, mime_types[ext])
-  end
-
-  def route_root_folder(conn, name) do
-    mime_types = %{
-      "ico" => "image/vnd.microsoft.icon",
-      "json" => "application/json",
-      "txt" => "text/plain",
-      "js" => "application/javascript"
-    }
-
-    ext = Regex.scan(~r/[\da-zA-Z\-\.]+\.([a-z]+)/, name) |> hd |> tl |> hd
-    send_static_file(conn, "", name, mime_types[ext])
-  end
-
-  # "Default" route that will get called when no other route is matched
-  match _ do
-    path = conn.path_info |> hd
-
-    # if this is file
-    if String.match?(path, ~r/[\S]+\.[a-zA-Z]/) do
-      route_root_folder(conn, path)
+      if File.exists?(file) do
+        conn
+        |> put_resp_header("content-type", "text/html; charset=utf-8")
+        |> send_file(200, file)
+      else
+        send_resp(conn, 404, "Not found")
+      end
     else
       send_resp(conn, 404, "Not found")
     end
