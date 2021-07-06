abef2555414e5faa372bc16178783ab95afa3612
Ilia <istudyatuni@gmail.com>
2021-07-06 19:55:29 +0300

    Remade static serving

--- server/lib/tasker/routers/web.ex
+++ server/lib/tasker/routers/web.ex
@@ -1,28 +1,17 @@
 defmodule Tasker.Routers.Web do
-  use Plug.Builder
+  use Plug.Router
 
   @static_folder Path.expand(Application.get_env(:tasker, :web_app_folder))
 
-  plug(Plug.Static,
-    at: "/",
-    from: @static_folder
-  )
+  plug(Plug.Static, at: "/", from: @static_folder)
+  plug(:match)
+  plug(:dispatch)
 
-  plug(:not_found)
+  get "/" do
+    send_file(conn, 200, "#{@static_folder}/index.html")
+  end
 
-  def not_found(conn, _) do
-    if conn.request_path == "/" do
-      file = "#{@static_folder}/index.html"
-
-      if File.exists?(file) do
-        conn
-        |> put_resp_header("content-type", "text/html; charset=utf-8")
-        |> send_file(200, file)
-      else
-        send_resp(conn, 404, "Not found")
-      end
-    else
-      send_resp(conn, 404, "Not found")
-    end
+  match _ do
+    send_resp(conn, 404, "Not found")
   end
 end
