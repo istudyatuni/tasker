c9bc09b0261b4592fb22186321d35669ca4f0638
Ilia <istudyatuni@gmail.com>
2021-08-21 09:34:32 +0300

    [svelte] Added baseUrl analogue

--- web-svelte/rollup.config.js
+++ web-svelte/rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import alias from '@rollup/plugin-alias';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -75,7 +76,13 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+    alias({
+      entries: [
+      	{ find: 'src', replacement: 'src' },
+      ]
+    }),
 	],
 	watch: {
 		clearScreen: false
