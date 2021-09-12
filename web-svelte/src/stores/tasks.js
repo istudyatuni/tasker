d54e4b4b90fe8cddeaa70a5ac1e640535c95d4d7
Ilia <istudyatuni@gmail.com>
2021-09-12 22:04:15 +0300

    [svelte] Improve store

--- web-svelte/src/stores/tasks.js
+++ web-svelte/src/stores/tasks.js
@@ -1,3 +1,25 @@
 import { writable } from 'svelte/store'
 
-export const tasks = writable([])
+// https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_stores#how_to_implement_a_store_contract_the_theory
+function tasksStore() {
+	const { subscribe, set, update } = writable([])
+
+	return {
+		subscribe,
+		set,
+		update: (id, task) => {
+			const i = data.findIndex(e => e.task_id === id)
+			if (i !== -1) {
+				return update(tasks => {
+					tasks[i] = task
+					return tasks
+				})
+			}
+
+			console.error('Not found task with id ', id)
+		},
+		push: (task) => update(tasks => tasks.push(task))
+	}
+}
+
+export const tasks = tasksStore()
