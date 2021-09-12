fa1f43f77022e6b03f857940772fd4741a377a70
Ilia <istudyatuni@gmail.com>
2021-09-12 23:15:32 +0300

    [svelte] Move task to TaskItem. Add showing more details

---
+++ web-svelte/src/components/TaskItem.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { FinishTask } from 'src/api/FinishTask.js'
+
+	export let task;
+
+	let opened = true;
+
+	function toggleDetails() {
+		opened = !opened;
+	}
+</script>
+
+<!-- header -->
+<div class="is-flex">
+	<img
+		src={`icons/${task.finished ? '' : 'in'}completed-task.svg`}
+		on:click={() => {
+			FinishTask(task.task_id, !task.finished)
+		}}
+		class="icon mr-4 pt-1 is-clickable"
+		width="50" height="50" alt=""
+		title="Finish task"
+	>
+	<div class="is-clickable"
+		on:click={toggleDetails}
+		title="Open details"
+	>
+		<p class="title is-5">{task.name}</p>
+		<p class="subtitle">{task.description}</p>
+	</div>
+</div>
+
+<!-- details -->
+{#if opened}
+	<div class="is-flex is-justify-content-space-between mt-5">
+		<div>
+			<p class="title is-4">{task.full_name}</p>
+			<p class="subtitle">{task.subject}</p>
+		</div>
+		<button class="button">Edit</button>
+	</div>
+	<hr>
+{/if}
--- web-svelte/src/components/TasksList.svelte
+++ web-svelte/src/components/TasksList.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { tasks } from 'src/stores/tasks.js'
-	import { FinishTask } from 'src/api/FinishTask.js'
+
+	import TaskItem from 'src/components/TaskItem.svelte'
 </script>
 
 {#if $tasks.length === 0}
@@ -9,20 +10,6 @@
 
 {#each $tasks as task}
 	<div class="box">
-		<!-- header -->
-		<div class="is-flex">
-			<img
-				src={`icons/${task.finished ? '' : 'in'}completed-task.svg`}
-				on:click={() => {
-					FinishTask(task.task_id, !task.finished)
-				}}
-				class="icon mr-3 pt-1 is-clickable"
-				width="50" height="50" alt=""
-			>
-			<div>
-				<p class="title is-5">{task.name}</p>
-				<p class="subtitle">{task.description}</p>
-			</div>
-		</div>
+		<TaskItem {task} />
 	</div>
 {/each}
