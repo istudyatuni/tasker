57790ea627fe39d27a2831b3c4afa5323e980902
Ilia <istudyatuni@gmail.com>
2021-09-14 21:32:01 +0300

    [svelte] Show if all tasks are finished

--- web-svelte/src/components/TasksList.svelte
+++ web-svelte/src/components/TasksList.svelte
@@ -6,13 +6,17 @@
 </script>
 
 {#if $tasks.length === 0}
-	<p class="notification is-success">Tasks list is empty</p>
-{/if}
+	<p class="notification is-success">The task list is empty</p>
+{:else if !$settings.show_finished && $tasks.every((task) => task.finished)}
+	<p class="notification is-success">All tasks are finished!</p>
+{:else}
 
-{#each $tasks as task}
-	{#if !task.finished || $settings.show_finished}
-		<div class="box">
-			<TaskItem {task} />
-		</div>
-	{/if}
-{/each}
+	{#each $tasks as task}
+		{#if !task.finished || $settings.show_finished}
+			<div class="box">
+				<TaskItem {task} />
+			</div>
+		{/if}
+	{/each}
+
+{/if}
