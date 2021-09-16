329333297177b494a3c524c194dd60ee5026ca22
Ilia <istudyatuni@gmail.com>
2021-09-16 08:06:10 +0300

    [svelte] Moved localKeyValueStore to separate file

--- web-svelte/src/stores/settings.js
+++ web-svelte/src/stores/settings.js
@@ -1,32 +1,3 @@
-import { writable } from 'svelte/store'
+import { localKeyValueStore } from 'src/stores/localKeyValueStore.js'
 
-function localKeyValueStore(name, initial = {}) {
-	const toString = (value) => JSON.stringify(value, null, 2)
-	const toObject = JSON.parse
-
-	if (localStorage.getItem(name) === null) {
-		localStorage.setItem(name, toString(initial))
-	}
-
-	const saved = toObject(localStorage.getItem(name))
-
-	const { subscribe, set, update } = writable(saved)
-
-	return {
-		subscribe,
-		set: (key, value) => update(data => {
-			data[key] = value
-			localStorage.setItem(name, toString(data))
-			return data
-		}),
-		delete: (key) => update(data => {
-			delete data[key]
-			localStorage.setItem(name, toString(data))
-			return data
-		})
-	}
-}
-
-// set: settings.set('key', 'value')
-// get: settings.key
 export const settings = localKeyValueStore('settings')
