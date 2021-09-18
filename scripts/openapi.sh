5cb3b238a2c6936713b7f9bbfd481e6f49ff345b
Ilia <istudyatuni@gmail.com>
2021-09-18 11:44:52 +0300

    Add script for downloading swagger-ui

---
+++ scripts/openapi.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Download latest swagger-ui and place in docs/openapi
+
+root='docs/openapi'
+releases='https://api.github.com/repos/swagger-api/swagger-ui/releases/latest'
+
+response=$(curl -s $releases)
+tag=$(echo $response | jq -r '.tag_name')
+tarball=$(echo $response | jq -r '.tarball_url')
+out_file="$tag.tar.gz"
+version_file=$root/release.txt
+
+echo -e "Current release $(cat $version_file 2> /dev/null)\n"
+echo -e "Downloading release $tag\n"
+
+wget --no-clobber -O $out_file $tarball
+
+tar xf $out_file -C $root
+mv $root/swagger-api-*/dist/* $root
+rm -rf $root/swagger-api-*
+
+echo $tag > $version_file
