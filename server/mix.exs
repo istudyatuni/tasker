8628398288dc3543d6ae713cb01dcc1e3f6166f1
Ilia <istudyatuni@gmail.com>
2021-09-19 17:24:29 +0300

    v0.3.2

--- server/mix.exs
+++ server/mix.exs
@@ -4,7 +4,7 @@ defmodule Tasker.MixProject do
   def project do
     [
       app: :tasker,
-      version: "0.3.0",
+      version: "0.3.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
