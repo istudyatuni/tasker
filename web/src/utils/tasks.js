096be77eeccc316744664499f0ec5148705712c2
Ilia <istudyatuni@gmail.com>
2021-09-19 14:34:47 +0300

    [svelte] Filter empty subjects

--- web/src/utils/tasks.js
+++ web/src/utils/tasks.js
@@ -1,7 +1,7 @@
 import { get } from 'svelte/store'
 
 export function getSubjects(tasks) {
-	let subjects = tasks.map(e => e.subject)
+	let subjects = tasks.map(e => e.subject).filter(e => e)
 
 	// Set for unique
 	return [...new Set(subjects)]
