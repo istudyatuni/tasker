dda1af1d425556fede8bc9ee994fc9b07d274fd2
Ilia <istudyatuni@gmail.com>
2022-01-11 16:26:24 +0300

    Update fs support message

--- web/src/components/OfflineNotification.svelte
+++ web/src/components/OfflineNotification.svelte
@@ -6,6 +6,11 @@
 	} from 'src/utils/fs.js'
 </script>
 
+<script>
+	// nbsp for showing this name in one line
+	const apiName = 'File System Access API'.replace(/ /g, '&nbsp;')
+</script>
+
 {#if isFsSupported()}
 	<div class="mt-2 notification is-success">
 		<p class="block">
@@ -17,9 +22,9 @@
 {:else}
 	<div class="mt-2 notification is-danger has-text-white is-size-5">
 		<p class="block">
-			Unfortunately, your browser does not support the
-			<code>File System Access API</code> for working with local files. You can
-			check which browsers support it on
+			Couldn't connect to server, tried to work offline, but unfortunately
+			your browser does not support the <code>{@html apiName}</code> for
+			working with local files. You can check which browsers support it on
 			<a href="https://caniuse.com/native-filesystem-api">this page</a>
 		</p>
 	</div>
