95cca4584dc022d00084d74d49220af2ab6c5e7c
Ilia <istudyatuni@gmail.com>
2022-01-13 17:26:29 +0300

    Move line with info into component, fix showing finished for filtered tasks

--- web/src/components/App.svelte
+++ web/src/components/App.svelte
@@ -2,13 +2,13 @@
 	import { LoadTasks } from 'src/api/LoadTasks.js'
 
 	import SettingsBox from 'src/components/SettingsBox.svelte'
+	import Statistics from 'src/components/Statistics.svelte';
 	import TaskInputModal from 'src/components/TaskInputModal.svelte'
 	import TasksList from 'src/components/TasksList.svelte'
 	import Notification from 'src/components/Notification.svelte'
 	import OfflineNotification from 'src/components/OfflineNotification.svelte'
 
 	import { settings } from 'src/stores/settings.js'
-	import { tasks } from 'src/stores/tasks.js'
 
 	import { edit } from 'src/utils/editor.js'
 	import { initSettings } from 'src/utils/defaultSettings.js'
@@ -19,31 +19,13 @@
 
 <script>
 	let promise = LoadTasks()
-
-	// except deleted
-	$: finished = $tasks.filter((t) => t.finished && !t.deleted).length
-	$: total = $tasks.filter((t) => !t.deleted).length
-	$: finished_percent = Math.round((finished * 100) / total)
-
-	$: deleted = $tasks.filter((t) => t.deleted).length
 </script>
 
 <main class="container is-max-desktop pt-5 pl-3 pr-3">
 	<h1 class="title">Tasks</h1>
 	{#if $settings.offline !== 'wait'}
 		<SettingsBox />
-
-		{#if !$settings.show_trash}
-			<p class="block">
-				Finished {finished}/{total} ({finished_percent}%)
-
-				{#if $settings.filterSubject}
-					, filtered by "{$settings.filterSubject}"
-				{/if}
-			</p>
-		{:else}
-			<p class="block">Deleted {deleted}</p>
-		{/if}
+		<Statistics />
 	{/if}
 
 	{#await promise}
---
+++ web/src/components/Statistics.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	import { settings } from 'src/stores/settings.js'
+	import { filtered as tasks } from 'src/stores/tasks.js'
+</script>
+
+<script>
+	// except deleted
+	$: finished = $tasks.filter((t) => t.finished && !t.deleted).length
+	$: total = $tasks.filter((t) => !t.deleted).length
+	$: finished_percent = Math.round((finished * 100) / total)
+
+	$: deleted = $tasks.filter((t) => t.deleted).length
+</script>
+
+{#if !$settings.show_trash}
+	<p class="block">
+		Finished {finished}/{total} ({finished_percent}%)
+
+		{#if $settings.filterSubject}
+			, filtered by "{$settings.filterSubject}"
+		{/if}
+	</p>
+{:else}
+	<p class="block">Deleted {deleted}</p>
+{/if}
