4a977c9bdd746011004b61b9fe9283cd6c56c362
Ilia <istudyatuni@gmail.com>
2022-01-14 11:53:43 +0300

    Add text help instead of title

--- web/src/components/Title.svelte
+++ web/src/components/Title.svelte
@@ -7,8 +7,8 @@
 	function tagColor(curr) {
 		return build_sha === curr ? 'success' : 'warning'
 	}
-	function tagTitle(curr) {
-		return build_sha === curr ? '' : 'Newer version is available, refresh page'
+	function isDisplay(curr) {
+		return build_sha !== curr
 	}
 </script>
 
@@ -20,13 +20,33 @@
 	Tasks
 	{#await promise then version}
 		{#if version}
-			<a
-				href="https://github.com/istudyatuni/tasker"
-				class="link tag is-{tagColor(version)} is-light is-normal"
-				title={tagTitle(version)}
-				tabindex="0">
-				{sha}
-			</a>
+			<div class="hover">
+				<a
+					href="https://github.com/istudyatuni/tasker"
+					class="link tag is-{tagColor(version)} is-light is-normal"
+					tabindex="0">
+					{sha}
+				</a>
+				<div
+					class="hover is-size-7 has-text-weight-normal"
+					class:is-hidden={!isDisplay(version)}>
+					Newer version is available, refresh page (ctrl+F5)
+				</div>
+			</div>
 		{/if}
 	{/await}
 </h1>
+
+<style>
+	div.hover {
+		display: inline;
+	}
+	div.hover > div {
+		display: none;
+	}
+	.hover:hover > div {
+		display: initial;
+		position: relative;
+		bottom: 0.25em;
+	}
+</style>
