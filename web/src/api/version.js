617b949f14ebd249b145438594f3db5eda19619d
Ilia <istudyatuni@gmail.com>
2022-01-14 19:54:23 +0300

    Fix retrieving SHA, now from gh-pages branch

--- web/src/api/version.js
+++ web/src/api/version.js
@@ -1,7 +1,12 @@
+function extractSha(message) {
+	const m = message.match(/tasker@(\w+)/)
+	return m.length > 1 ? m[1] : ''
+}
+
 export async function getVersion() {
-	const response = await fetch('https://api.github.com/repos/istudyatuni/tasker/commits?per_page=1')
+	const response = await fetch('https://api.github.com/repos/istudyatuni/tasker/commits?per_page=1&sha=gh-pages')
 	if (response.ok) {
-		return (await response.json())[0].sha
+		return extractSha((await response.json())[0].commit.message)
 	}
 	return ''
 }
