68033c9322626a47a3340bfa4dca9206a654189b
Ilia <istudyatuni@gmail.com>
2022-01-15 20:54:57 +0300

    More docs

--- web/src/stores/settings.js
+++ web/src/stores/settings.js
@@ -1,3 +1,19 @@
 import { localStore } from 'svelte-storages'
 
+/**
+ * Settings
+ *
+ * Keys
+ *
+ *  - `show_finished`
+ *  - `show_trash`
+ *  - `strict_line_breaks`
+ *  - `editor`
+ *     - `state`
+ *     - `task`
+ *  - `offline`
+ *  - `filterSubject`
+ *
+ * @type {Writable}
+ */
 export const settings = localStore('settings')
